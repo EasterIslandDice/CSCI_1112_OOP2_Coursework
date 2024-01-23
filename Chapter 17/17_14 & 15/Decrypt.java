63d0f27136878eb0037062e8dc49262b927c82ee
EasterIslandDice <150847836+EasterIslandDice@users.noreply.github.com>
2024-01-23 10:22:17 -0700

    Add files via upload

---
+++ Chapter 17/17_14 & 15/Decrypt.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.io.*;
+
+class Untitled {
+	public static void main(String[] args) throws IOException{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter encrypted file name:");
+		String inputFile = input.next();
+		System.out.println("Please enter decrypted file name:");
+		String outputFile = input.next();
+		
+		input.close();
+		
+		try(
+			RandomAccessFile sourceFile = new RandomAccessFile(inputFile, "r");
+			RandomAccessFile targetFile = new RandomAccessFile(outputFile, "rw");
+		){
+			int i = 0;
+			sourceFile.seek(0);
+			while((i = sourceFile.read()) != -1){
+				targetFile.write(((byte)i) - 5);
+			}
+		}
+	}
+}
